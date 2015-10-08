1fe58062b8059a658d8d9f6462d90a264091803d
Duke-Mou <leodouglasduke@gmail.com>
2015-10-08 23:50:17 +0800

    storyboard update

---
+++ LOOfficeAssistant/LOOfficeAssistant/Sections/LOProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  LOProfileViewController.h
+//  
+//
+//  Created by MouXiangyang on 15/10/8.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOProfileViewController : UITableViewController
+
+@end
